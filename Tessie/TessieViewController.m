ee7c19c6c2da609dd36f46228e2b454f36c684bc
Jens Schmidt <jens@brightroll.com>
2015-04-10 23:27:24 -0700

    added commands to open/close sunroof

--- Tessie/TessieViewController.m
+++ Tessie/TessieViewController.m
@@ -17,6 +17,8 @@
     NSMutableArray *tableData;
     NSMutableArray *commandPhrases;
     NSArray * cmdReset;
+    NSArray * cmdBottom;
+    NSArray * cmdTop;
 }
 
 @end
@@ -24,31 +26,39 @@
 @implementation TessieViewController
 
 
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+
     // reset sequence to move mouse the the top left and click, to close any open dialogs
     cmdReset = [NSArray arrayWithObjects:
-                @"u127", @"l127", @"u127", @"l127", @"u127",
-                @"l127", @"u127", @"l127", @"u127", @"l127",
-                @"u127", @"l127", @"c", @"w10", nil];
-    
+                @"l127", @"l127", @"l127", @"l127", @"l127", @"l127", @"l127", @"l127",
+                @"u127", @"u127", @"u127", @"u127", @"u127", @"u127", @"u127", @"u127",  @"u127", @"u127",
+                @"c", @"w200", nil];
+
+    cmdBottom = [NSArray arrayWithObjects:
+                 @"d127", @"d127", @"d127", @"d127", @"d127", @"d127", @"d127", @"d127", @"d127", @"d127",
+                 nil];
+
+    cmdTop = [NSArray arrayWithObjects:
+              @"u127", @"u127", @"u127", @"u127", @"u127", @"u127", @"u127", @"u127",  @"u127", @"u127",
+            nil];
+
+
     tableData = [NSMutableArray array];
-    
+
     activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     UIBarButtonItem *barButton = [[UIBarButtonItem alloc] initWithCustomView:activityIndicator];
     self.navigationItem.rightBarButtonItem = barButton;
     self.navigationItem.hidesBackButton = NO;
-    
+
     bleShield = [[BLE alloc] init];
     [bleShield controlSetup];
     bleShield.delegate = self;
-    
+
     self.fliteController = [[OEFliteController alloc] init];
     self.slt = [[Slt alloc] init];
-    
+
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, .5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         [self connectToTesla];
     });
@@ -71,20 +81,18 @@
 
 - (void)connectToTesla
 {
-    if (bleShield.activePeripheral)
-        if(bleShield.activePeripheral.state == CBPeripheralStateConnected)
-        {
-            [[bleShield CM] cancelPeripheralConnection:[bleShield activePeripheral]];
-            return;
-        }
-    
-    if (bleShield.peripherals)
-        bleShield.peripherals = nil;
-    
+    if (bleShield.activePeripheral && bleShield.activePeripheral.state == CBPeripheralStateConnected)
+    {
+        [[bleShield CM] cancelPeripheralConnection:[bleShield activePeripheral]];
+        return;
+    }
+
+    if (bleShield.peripherals) bleShield.peripherals = nil;
+
     [bleShield findBLEPeripherals:3];
-    
+
     [NSTimer scheduledTimerWithTimeInterval:(float)3.0 target:self selector:@selector(connectionTimer:) userInfo:nil repeats:NO];
-    
+
     [activityIndicator startAnimating];
     self.navigationItem.leftBarButtonItem.enabled = NO;
 }
@@ -106,11 +114,11 @@ NSTimer *rssiTimer;
     NSLog(@"bleDidDisconnect");
     [tableData  removeAllObjects];
     [_tableView reloadData];
-    
+
     [self.navigationItem.leftBarButtonItem setTitle:@"Connect"];
     [activityIndicator stopAnimating];
     self.navigationItem.leftBarButtonItem.enabled = YES;
-    
+
     [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
 }
 
@@ -119,12 +127,12 @@ NSTimer *rssiTimer;
 {
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"commands" ofType:@"json"];
     NSData *data = [NSData dataWithContentsOfFile:filePath];
-    
+
     //parse command data
     NSError* error;
     NSDictionary* commands = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
     commandPhrases = [NSMutableArray arrayWithObjects: nil];
-    
+
     for (NSDictionary * command in commands) {
         NSString * title = [command objectForKey:@"title"];
         NSLog(@"Adding Command: %@", title);
@@ -134,25 +142,25 @@ NSTimer *rssiTimer;
 
     [_tableView setContentOffset:CGPointMake(0, CGFLOAT_MAX)];
     [_tableView reloadData];
-    
+
     OELanguageModelGenerator *lmGenerator = [[OELanguageModelGenerator alloc] init];
 
-    
+
     NSString *name = @"TeslaCommands";
     NSError *err = [lmGenerator generateLanguageModelFromArray:commandPhrases withFilesNamed:name forAcousticModelAtPath:[OEAcousticModel pathToModel:@"AcousticModelEnglish"]]; // Change "AcousticModelEnglish" to "AcousticModelSpanish" to create a Spanish language model instead of an English one.
-    
+
     if(err == nil) {
-        
+
         lmPath = [lmGenerator pathToSuccessfullyGeneratedLanguageModelWithRequestedName:@"TeslaCommands"];
         dicPath = [lmGenerator pathToSuccessfullyGeneratedDictionaryWithRequestedName:@"TeslaCommands"];
-        
+
     } else {
         NSLog(@"Error: %@",[err localizedDescription]);
     }
-    
+
     self.openEarsEventsObserver = [[OEEventsObserver alloc] init];
     [self.openEarsEventsObserver setDelegate:self];
-    
+
 }
 
 -(void) bleDidConnect
@@ -162,7 +170,7 @@ NSTimer *rssiTimer;
     self.navigationItem.leftBarButtonItem.enabled = YES;
     self.voiceButton.enabled = YES;
     [self.navigationItem.leftBarButtonItem setTitle:@"Disconnect"];
-    
+
     NSLog(@"bleDidConnect");
 }
 
@@ -190,9 +198,9 @@ NSTimer *rssiTimer;
     if ([[OEPocketsphinxController sharedInstance] isListening]) {
         return;
     }
-    
+
     self.voiceButton.enabled = FALSE;
-    
+
     [[OEPocketsphinxController sharedInstance] setActive:TRUE error:nil];
     [[OEPocketsphinxController sharedInstance] startListeningWithLanguageModelAtPath:lmPath dictionaryAtPath:dicPath acousticModelAtPath:[OEAcousticModel pathToModel:@"AcousticModelEnglish"] languageModelIsJSGF:NO];
 }
@@ -220,7 +228,9 @@ NSTimer *rssiTimer;
     NSString * commandTitle = [command objectForKey:TITLE_STR];
     NSArray *commands = [command objectForKey:COMMANDS_STR];
     NSLog(@"Sending Command: \"%@\"", commandTitle);
-    [self.fliteController say:commandTitle withVoice:self.slt];
+    // TODO: Generalize / add "confirmation" to commands.json
+    NSString * confirmation = [commandTitle stringByReplacingOccurrencesOfString:@"Show" withString:@"Showing"];
+    [self.fliteController say:confirmation withVoice:self.slt];
     [self bleSendCommands:commands];
 }
 
@@ -228,19 +238,27 @@ NSTimer *rssiTimer;
 -(void)bleSendCommands:(NSArray*) commands
 {
     dispatch_async(dispatch_get_global_queue( 0, 0), ^(void){
-     for (NSString * command in commands) {
-         // todo: clean this up, encapsulate if possible.
-         if ([@"reset" isEqualToString:command]) {
-             for (NSString * cmdString in cmdReset) {
-                 [self bleSendCommand:cmdString];
-             }
-         } else {
-             [self bleSendCommand:command];
-         }
-        // hack
-        [NSThread sleepForTimeInterval:.05];
-     }
-   });
+        for (NSString * command in commands) {
+            // todo: clean this up, encapsulate if possible.
+            if ([@"reset" isEqualToString:command]) {
+                for (NSString * cmdString in cmdReset) {
+                    [self bleSendCommand:cmdString];
+                }
+            } else if ([@"top" isEqualToString:command]) {
+                for (NSString * cmdString in cmdTop) {
+                    [self bleSendCommand:cmdString];
+                }
+            } else if ([@"bottom" isEqualToString:command]) {
+                for (NSString * cmdString in cmdBottom) {
+                    [self bleSendCommand:cmdString];
+                }
+            } else {
+                [self bleSendCommand:command];
+            }
+            // hack
+            [NSThread sleepForTimeInterval:.05];
+        }
+    });
 }
 
 
@@ -256,19 +274,19 @@ NSTimer *rssiTimer;
 }
 
 - (void) pocketsphinxDidReceiveHypothesis:(NSString *)hypothesis recognitionScore:(NSString *)recognitionScore utteranceID:(NSString *)utteranceID {
-    [[OEPocketsphinxController sharedInstance] stopListening];
-    self.voiceButton.enabled = YES;
-    
+
     NSLog(@"Heard speech: \"%@\", score: %@, id: %@", hypothesis, recognitionScore, utteranceID);
-    
+
     [commandPhrases enumerateObjectsUsingBlock:^(id phrase, NSUInteger idx, BOOL *stop)
-    {
-        NSRange range = [hypothesis rangeOfString:phrase];
-        if (range.location != NSNotFound) {
-            NSDictionary *dict = [tableData objectAtIndex:idx];
-            [self sendCommand:dict];
-        }
-    }];
+     {
+         NSRange range = [hypothesis rangeOfString:phrase];
+         if (range.location != NSNotFound) {
+             NSDictionary *dict = [tableData objectAtIndex:idx];
+             [self sendCommand:dict];
+             [[OEPocketsphinxController sharedInstance] stopListening];
+             self.voiceButton.enabled = YES;
+         }
+     }];
 }
 
 - (void) pocketsphinxDidStartListening {
